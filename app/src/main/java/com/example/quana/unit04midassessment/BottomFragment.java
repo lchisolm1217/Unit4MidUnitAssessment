18fca6555ee85e2333f5eb24a9ef57fd26473818
lchisolm1217 <laquanachisolm@ac.c4q.nyc>
2017-12-20 22:08:54 -0500

    part02 not complete

--- app/src/main/java/com/example/quana/unit04midassessment/BottomFragment.java
+++ app/src/main/java/com/example/quana/unit04midassessment/BottomFragment.java
@@ -47,7 +47,7 @@ public class BottomFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
+-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
