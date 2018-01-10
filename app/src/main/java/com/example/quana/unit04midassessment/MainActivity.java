6bfa39bec05bdd9628fc8ace0f5dca81380266fa
lchisolm1217 <laquanachisolm@ac.c4q.nyc>
2018-01-09 19:58:55 -0500

    pushing assessment

--- app/src/main/java/com/example/quana/unit04midassessment/MainActivity.java
+++ app/src/main/java/com/example/quana/unit04midassessment/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+    MainFragment1 mainFragment1 = new MainFragment1();
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+    fragmentTransaction.replace(R.id.fragmentcontainer,mainFragment1);
+    fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/quana/unit04midassessment/MainFragment.java
+++ app/src/main/java/com/example/quana/unit04midassessment/MainFragment.java
@@ -1,113 +1,113 @@
-package com.example.quana.unit04midassessment;
-
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MainFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MainFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MainFragment extends Fragment {
-   private Button button;
-   private OnFragmentInteractionListener mListener;
-
-    public MainFragment() {
-
-    }
-
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MainFragment newInstance(String param1, String param2) {
-        MainFragment fragment = new MainFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
-        Button button1 = button.findViewById(R.id.fragment_button);
-
-        return view;
-
-
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-
-    button.setOnClickListener(new.View.OnClickListener() {
-
-    @Override
-    public void onClick(View v) {
-            Fragment myfragment = new Fragment();
-            FragmentTransaction transaction = getFragmentManager.beginTransaction();
-            transaction.replace(R.id.fragment_button, myfragment);
-            transaction.addToBackStack(DisplayFragment);
-            transaction.commit();
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
-}
+//package com.example.quana.unit04midassessment;
+//
+//import android.content.Context;
+//import android.net.Uri;
+//import android.os.Bundle;
+//import android.support.v4.app.Fragment;
+//import android.support.v4.app.FragmentManager;
+//import android.support.v4.app.FragmentTransaction;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.Button;
+//
+//
+///**
+// * A simple {@link Fragment} subclass.
+// * Activities that contain this fragment must implement the
+// * {@link MainFragment.OnFragmentInteractionListener} interface
+// * to handle interaction events.
+// * Use the {@link MainFragment#newInstance} factory method to
+// * create an instance of this fragment.
+// */
+//public class MainFragment extends Fragment {
+//   private Button button;
+//   private OnFragmentInteractionListener mListener;
+//
+//    public MainFragment() {
+//
+//    }
+//
+//
+//    /**
+//     * Use this factory method to create a new instance of
+//     * this fragment using the provided parameters.
+//     *
+//     * @param param1 Parameter 1.
+//     * @param param2 Parameter 2.
+//     * @return A new instance of fragment MainFragment.
+//     */
+//    // TODO: Rename and change types and number of parameters
+//    public static MainFragment newInstance(String param1, String param2) {
+//        MainFragment fragment = new MainFragment();
+//        Bundle args = new Bundle();
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//
+//        }
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        View view = inflater.inflate(R.layout.fragment_main, container, false);
+//        Button button1 = button.findViewById(R.id.fragment_button);
+//
+//        return view;
+//
+//
+//    }
+//
+//    // TODO: Rename method, update argument and hook method into UI event
+//
+//    button.setOnClickListener(new.View.OnClickListener() {
+//
+//    @Override
+//    public void onClick(View v) {
+//            Fragment myfragment = new Fragment();
+//            FragmentTransaction transaction = getFragmentManager.beginTransaction();
+//            transaction.replace(R.id.fragment_button, myfragment);
+//            transaction.addToBackStack(DisplayFragment);
+//            transaction.commit();
+//        }
+//    }
+//
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
+//
+//    /**
+//     * This interface must be implemented by activities that contain this
+//     * fragment to allow an interaction in this fragment to be communicated
+//     * to the activity and potentially other fragments contained in that
+//     * activity.
+//     * <p>
+//     * See the Android Training lesson <a href=
+//     * "http://developer.android.com/training/basics/fragments/communicating.html"
+//     * >Communicating with Other Fragments</a> for more information.
+//     */
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        void onFragmentInteraction(Uri uri);
+//    }
+//}
